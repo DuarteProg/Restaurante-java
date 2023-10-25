586cd94f5736a2dfa5ada009f22cc8cd637d5791
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-10-25 14:41:30 -0300

    projeto iniciado com duas entidades já criadas

---
+++ src/main/java/com/duartefood/DuartefoodApplication.java
@@ -0,0 +1,13 @@
+package com.duartefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuartefoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuartefoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duarte/duartefood/DuartefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duarte.duartefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuartefoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
